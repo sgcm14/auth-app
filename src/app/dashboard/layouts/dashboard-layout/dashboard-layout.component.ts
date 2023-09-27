3a722813f4673042a549bb4337c2907d7065bb2b
sgcm14 <sgcm14@gmail.com>
2023-09-26 23:26:48 -0500

    subiendo auth

---
+++ src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -0,0 +1,21 @@
+import { Component, computed, inject } from '@angular/core';
+import { AuthService } from 'src/app/auth/services/auth.service';
+
+@Component({
+  templateUrl: './dashboard-layout.component.html',
+  styleUrls: ['./dashboard-layout.component.css']
+})
+export class DashboardLayoutComponent {
+
+  private authService = inject(AuthService);
+
+  public user = computed(() => this.authService.currectUser());
+
+  // get user(){
+  //   return this.authService.currectUser();
+  // }
+
+  onLogout(){
+    this.authService.logout();
+  }
+}
---
+++ src/environments/environments.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  //baseUrl: 'http://localhost:3000'
+  baseUrl:'https://auth-mean-production-sgcm14.up.railway.app'
+}
